335ba6cb4ad4e44a2a9dc5b9b2de6d2fb4d30d3b
ultrasonicsoft <uvbrainsolutions@outlook.com>
2016-05-14 08:45:46 +0530

    Refactored components

--- app/components/dashboard/dashboard.component.ts
+++ app/components/dashboard/dashboard.component.ts
@@ -2,6 +2,6 @@ import {Component} from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>Jai Ganesh</h1>'
+    templateUrl : 'app/components/dashboard/dashboard.html'
 })
 export class AppComponent { }
