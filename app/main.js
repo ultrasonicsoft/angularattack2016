8ddd2de8afec3058d87f4de956afae2d16b095cc
ultrasonicsoft <uvbrainsolutions@outlook.com>
2016-05-14 09:57:40 +0530

    enabledProdMode

--- app/main.js
+++ app/main.js
@@ -1,5 +1,7 @@
 "use strict";
 var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
 var dashboard_component_1 = require('./components/dashboard/dashboard.component');
+var core_1 = require('@angular/core');
+core_1.enableProdMode();
 platform_browser_dynamic_1.bootstrap(dashboard_component_1.AppComponent);
 //# sourceMappingURL=main.js.map
--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,6 @@
 import {bootstrap}    from '@angular/platform-browser-dynamic';
 import {AppComponent} from './components/dashboard/dashboard.component';
+import {enableProdMode}    from '@angular/core';
 
+enableProdMode()
 bootstrap(AppComponent);
